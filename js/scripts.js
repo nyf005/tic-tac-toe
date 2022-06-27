e68232befe95683858abc550c77709593e7f431b
Fabrice N'DRI <nyf005@gmail.com>
2022-06-27 23:37:13 +0000

    Setup  app layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+// Rule of thumb:
+// if you only ever need ONE of something (gameBoard, displayController), use a module.
+// If you need multiples of something (players!), create them with factories.
+
+const gameBoard = (() => {
+  let _gameboard = [];
+
+  const setSquare = (index, symbol) => {
+    if (index < 0 || index > _gameboard.length) return;
+  };
+
+  const resetGameBoard = () => {};
+
+  return { setSquare, resetGameBoard };
+})();
+
+const displayController = (() => {})();
+
+const Player = (symbol) => {
+  let _playerSymbol = symbol;
+
+  const getSymbol = () => {
+    return _playerSymbol;
+  };
+
+  return { getSymbol };
+};
