7706514fe485b1b9fe87dbf7ea96395b520a55ed
Fabrice N'DRI <nyf005@gmail.com>
2022-06-28 23:04:12 +0000

    Create Player object

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,6 @@
 // if you only ever need ONE of something (gameBoard, displayController), use a module.
 // If you need multiples of something (players!), create them with factories.
 
-const gameBoard = (() => {
-  let _gameboard = [];
-
-  const setSquare = (index, symbol) => {
-    if (index < 0 || index > _gameboard.length) return;
-  };
-
-  const resetGameBoard = () => {};
-
-  return { setSquare, resetGameBoard };
-})();
-
-const displayController = (() => {})();
-
 const Player = (symbol) => {
   let _playerSymbol = symbol;
 
