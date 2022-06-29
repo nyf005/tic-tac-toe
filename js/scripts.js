cabf68443c7f690a596ded68160af41af712f37e
Fabrice N'DRI <nyf005@gmail.com>
2022-06-29 01:08:29 +0000

    Add index of user choice to gameboard array

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,62 @@ const Player = (symbol) => {
 };
 
 const gameBoard = (() => {
+  // odd number for pX and even number for pO
   let _gameboard = [];
 
-  const setSquare = (index, player) => {
-    if (index < 1 || index > 9) return;
+  // Add index of square chosen by player to array
+  const setChoice = (index) => {
+    if (!(index > 0 && index < 10) || _gameboard.includes(index)) return;
+    _gameboard.push(index);
   };
 
   const resetGameBoard = () => {};
 
-  return { setSquare, resetGameBoard };
+  return { setChoice, resetGameBoard };
 })();
 
 const displayController = (() => {
   // Get display elements
-  const _squares = document.querySelectorAll(".square");
   const _message = document.querySelector("#message p");
 
   // Display who's turn it is
   const showTurn = (player) => {
-    _message.textContent = `Player ${player.getSymbol()}'s turn`;
+    _message.textContent = `Player ${player.getSymbol().toUpperCase()}'s turn`;
   };
 
-  const showSymbol = (player) => {};
+  //
+  const addSymbolToBoard = (square, player) => {
+    if (square.textContent != "") return;
+    square.textContent = player.getSymbol();
+    square.style.backgroundColor =
+      player.getSymbol() == "x" ? "#ffc200" : "#fa5c0c";
+  };
 
-  return { showTurn, showSymbol };
+  return { showTurn, addSymbolToBoard };
 })();
 
 const gameController = (() => {
-  const _playerX = Player("X");
-  const _playerO = Player("O");
+  const _squares = document.querySelectorAll(".square");
+  const _winningCOnditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  const _playerX = Player("x");
+  const _playerO = Player("o");
 
-  displayController.showTurn(_playerO);
+  displayController.showTurn(_playerX);
+
+  _squares.forEach((square) => {
+    square.addEventListener("click", () => {
+      let index = square.getAttribute("data-index");
+      gameBoard.setChoice(index);
+      displayController.addSymbolToBoard(square, _playerO);
+    });
+  });
 })();
